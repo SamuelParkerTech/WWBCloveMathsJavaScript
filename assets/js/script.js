d841087ad4cff220ceb32eb2c727caa3b827e9a0
Samuel Parker <samuelparkertech@gmail.com>
2024-06-21 13:02:02 +0000

    added javascript functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// Wait for the DOM to finish loading before running the game
+//Get the button elements and add event listeners to them
+
+document.addEventListener("DOMcontentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!";)
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`you clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame() {}
 
 
@@ -5,8 +23,8 @@ function runGame() {}
 function checkAnswer() {}
 
 
-function calculateCorrectAnswer() {}
 
+function calculateCorrectAnswer() {}
 
 
 
