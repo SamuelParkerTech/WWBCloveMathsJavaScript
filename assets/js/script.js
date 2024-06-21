1fc8b4bd0fa3975419dedc645dc79d5c931fa59f
Samuel Parker <samuelparkertech@gmail.com>
2024-06-21 14:20:19 +0000

    addition game is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,8 +48,10 @@ function checkAnswer() {
 
     if (isCorrect){
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`AWWW.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1]);
 }
@@ -74,14 +76,20 @@ function calculateCorrectAnswer() {
 }
 
 
+/** Gets the current score from the DOM and increments it by 1
+ */
+function incrementScore() {
 
-function incrementScore() {}
+let oldScore =parseInt(document.getElementById("score").innerText);
+document.getElementById("score").innerText = ++oldScore;
+}
 
-
-
-function incrementWrongAnswer() {}
-
-
+/** Gets the current tally of incorrect from the DOM and increments it by 1
+ */
+function incrementWrongAnswer() {
+let oldScore =parseInt(document.getElementById("incorrect").innerText);
+document.getElementById("incorrect").innerText = ++oldScore;
+}
 
 function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
